8338eb2e7973c36753c44d63cddebeae29e194bd
NicFontana <niccolo.fontana@student.unife.it>
2018-03-06 18:32:58 +0100

    Aggiornata ancora l'implementazione del server: ora viene creata una sola socket per IPv6. Se un client si connette tramite socket IPv4, la comunicazione avverrà tramite indirizzo IPv6-v4Mapped.

--- server.py
+++ server.py
@@ -21,41 +21,31 @@ def child():
 	os._exit(0)
 
 
-# The server will listen to the first address family available.
-# On most of IPv6-ready systems, IPv6 will take precedence and the server may not accept IPv4 traffic.
-for res in socket.getaddrinfo(None, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
-
-	(family, type, proto, canonname, sockaddr) = res
-
-	try:
-		# Create the socket
-		ss = socket.socket(family, type, proto)
-	except OSError:
-		ss = None
-		continue
-	try:
-		# Set the SO_REUSEADDR flag in order to tell the kernel to reuse the socket even if it's in a TIME_WAIT state,
-		# without waiting for its natural timeout to expire.
-		# This is because sockets in a TIME_WAIT state can’t be immediately reused.
-		ss.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-		# Bind the local address (sockaddr) to the socket (ss)
-		ss.bind(sockaddr)
-
-		# Transform the socket in a passive socket and
-		# define a queue of SOMAXCONN possible connection requests
-		ss.listen(socket.SOMAXCONN)
-	except OSError:
-		ss = None
-		continue
-
-	break
-
-if ss is None:
-	print('Could not open socket')
+try:
+	# Create the socket
+	ss = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+except OSError as e:
+	print(f'Can\'t create the socket: {e}')
 	sys.exit(socket.error)
 
-print(f'Server {sockaddr[0]} listening on port {sockaddr[1]}...')
+try:
+	# Set the SO_REUSEADDR flag in order to tell the kernel to reuse the socket even if it's in a TIME_WAIT state,
+	# without waiting for its natural timeout to expire.
+	# This is because sockets in a TIME_WAIT state can’t be immediately reused.
+	ss.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+	# Bind the local address (sockaddr) to the socket (ss)
+	ss.bind(('', PORT))
+
+	# Transform the socket in a passive socket and
+	# define a queue of SOMAXCONN possible connection requests
+	ss.listen(socket.SOMAXCONN)
+except OSError:
+	print(f'Can\'t handle the socket: {OSError}')
+	sys.exit(socket.error)
+
+
+print(f'Server {ss.getsockname()[0]} listening on port {ss.getsockname()[1]}...')
 
 while True:
 	# Put the passive socket on hold for connection requests
@@ -67,5 +57,3 @@ while True:
 	# ----------------------------------
 
 	sd.close()
-
-
--- serverV2.py
+++ serverV2.py
@@ -3,17 +3,14 @@
 import socket
 import sys
 import os
-import selectors
 
 PORT = 3000
-sel = selectors.DefaultSelector()
 
 
 def child():
 	(client, client_port) = socket.getnameinfo(clientaddr, socket.NI_NUMERICHOST)
-	print(f'Client {client} on port {client_port}. Child PID: {os.getpid()}.\n')
-	ss4.close()
-	ss6.close()
+	print(f'Client {client} on port {client_port}. Child PID: {os.getpid()}\n')
+	ss.close()
 	while True:
 		data = sd.recv(1024)
 		print(f'Received: {data}')
@@ -24,70 +21,49 @@ def child():
 	os._exit(0)
 
 
-try:
-	# Create the sockets
-	ss4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	ss6 = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-except OSError:
-	print(f'Can\'t create the sockets: {OSError}')
+# The server will listen to the first address family available.
+# On most of IPv6-ready systems, IPv6 will take precedence and the server may not accept IPv4 traffic.
+for res in socket.getaddrinfo(None, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
+
+	(family, type, proto, canonname, sockaddr) = res
+
+	try:
+		# Create the socket
+		ss = socket.socket(family, type, proto)
+	except OSError:
+		ss = None
+		continue
+	try:
+		# Set the SO_REUSEADDR flag in order to tell the kernel to reuse the socket even if it's in a TIME_WAIT state,
+		# without waiting for its natural timeout to expire.
+		# This is because sockets in a TIME_WAIT state can’t be immediately reused.
+		ss.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+		# Bind the local address (sockaddr) to the socket (ss)
+		ss.bind(sockaddr)
+
+		# Transform the socket in a passive socket and
+		# define a queue of SOMAXCONN possible connection requests
+		ss.listen(socket.SOMAXCONN)
+	except OSError:
+		ss = None
+		continue
+
+	break
+
+if ss is None:
+	print('Could not open socket')
 	sys.exit(socket.error)
 
-try:
-	# Set the SO_REUSEADDR flag in order to tell the kernel to reuse the socket even if it's in a TIME_WAIT state,
-	# without waiting for its natural timeout to expire.
-	# This is because sockets in a TIME_WAIT state can’t be immediately reused.
-	ss4.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	ss6.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-	# Bind the local address to the sockets (ss4, ss6)
-	ss4.bind(('', PORT))
-	ss6.bind(('', PORT))
-
-	# Transform the socket in a passive socket and
-	# define a queue of SOMAXCONN possible connection requests
-	ss4.listen(socket.SOMAXCONN)
-	ss6.listen(socket.SOMAXCONN)
-
-	# Set the sockets to be non-blocking in order to have
-	# a non-blocking accept()
-	ss4.setblocking(False)
-	ss6.setblocking(False)
-except OSError:
-	print(f'Can\'t handle the sockets: {OSError}')
-	sys.exit(socket.error)
-
-# Register the socket ss4 for selection, monitoring it for Input events.
-sel.register(ss4, selectors.EVENT_READ)
-# Register the socket ss6 for selection, monitoring it for Input events.
-sel.register(ss6, selectors.EVENT_READ)
-
-print('Server listening...')
+print(f'Server {sockaddr[0]} listening on port {sockaddr[1]}...')
 
 while True:
-	# Wait until some registered file objects become ready, or the timeout expires
-	# This returns a list of (key, events) tuples, one for each ready file object
-	#
-	# We use the select in order to wait incoming connections on both the sockets simultaneously:
-	# once the passive socket has a connection ready (the client has called the connect())
-	# the select "is triggered" and the accept will be non-blocking
-	events = sel.select()
+	# Put the passive socket on hold for connection requests
+	(sd, clientaddr) = ss.accept()
 
-	for (key, mask) in events:
-		# • key is the SelectorKey instance corresponding to a ready file object
-		# A SelectorKey is a namedtuple used to associate a file object to its underlying
-		# file descriptor, selected event mask and attached data
-		# • mask is a bitmask of events ready on this file object.
-		sock = key.fileobj
+	pid = os.fork()
+	if pid == 0:  # --------FIGLIO-------
+		child()
+	# ----------------------------------
 
-		# Put the passive socket on hold for connection requests
-		(sd, clientaddr) = sock.accept()
-
-		# Explicitly set with the desired socket options.(Best practice)
-		sd.setblocking(True)
-
-		pid = os.fork()
-		if pid == 0:  # --------FIGLIO-------
-			child()
-		# ----------------------------------
-
-		sd.close()
+	sd.close()
