7c94c48a1f4c1eb444f24c96f7c1afaf037be77a
NicFontana <niccolo.fontana@student.unife.it>
2018-03-13 12:30:45 +0100

    Aggiustato lo schema.sql per consentire le NATURAL JOIN tra peers e files

--- database/schema.sql
+++ database/schema.sql
@@ -12,13 +12,13 @@ CREATE TABLE files (
     id integer PRIMARY KEY,
     file_md5 char(32) NOT NULL,
     file_name char(100) NOT NULL,
-    download_count integer DEFAULT 0
+    download_count integer DEFAULT 0 NOT NULL
 );
 
 DROP TABLE IF EXISTS files_peers;
 CREATE TABLE files_peers (
-    file_id integer NOT NULL,
-    peer_session_id char(16) NOT NULL,
+    id integer NOT NULL,
+    session_id char(16) NOT NULL,
     PRIMARY KEY (file_id, peer_session_id),
     FOREIGN KEY (file_id) REFERENCES files (id) ON DELETE CASCADE,
     FOREIGN KEY (peer_session_id) REFERENCES peers (session_id) ON DELETE CASCADE
